21afb93427dfa68b6c79588680231adb6d8195cf
Aaron Washburn <aaron@it-kutch.com>
2015-02-09 17:21:53 +0000

    Add sections new and show

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,6 +1,24 @@
 class Instructor::SectionsController < ApplicationController
+	
 	def new
 		@section = Section.new
 		@course = Course.find(params[:course_id])
+	end
+
+	def create
+		@course = Course.find(params[:course_id])
+    @section = @course.sections.create(section_params)
+
+		if @section.valid?
+			redirect_to instructor_course_path(@course)
+		else
+			render :new, :status => :unprocessable_entity
+		end
+	end
+
+	private
+
+	def section_params
+		params.require(:section).permit(:title)
 	end
 end
