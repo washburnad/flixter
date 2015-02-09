dea9e54b1294977ddc90bf220ce1d9394513b9f8
Aaron Washburn <aaron@it-kutch.com>
2015-02-09 15:21:05 +0000

    Generate courses controller

--- test/controllers/instructor/courses_controller_test.rb
+++ test/controllers/instructor/courses_controller_test.rb
@@ -1,42 +1,42 @@
 require 'test_helper'
 
 class Instructor::CoursesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # include Devise::TestHelpers
+  # include Warden::Test::Helpers  
+  # Warden.test_mode!
 
+  # def teardown                                         
+  #   Warden.test_reset!                                 
+  # end 
+  
   setup do
   	@user = FactoryGirl.create(:user)
     sign_in @user
   end
 
   test "course new page"	do
-  	# user = FactoryGirl.create(:user)
-  	# sign_in user
   	get :new
 
   	assert_response :success
   end
 
   test "course show page" do
-  	# user = FactoryGirl.create(:user)
-  	# sign_in user
   	course = FactoryGirl.create(:course)
-
   	get :show, :id => course.id
 
-  	assert true
+  	assert_response :success
   end
 
   # Trying to test that page redirects to sign in if attempt is made to 
   # create a course and no user is signed in.  Triggering uncaught throw: warden
-  #
-  # test "course new page redirect to sign in" do
-  #   sign_out @user
+  
+  test "course new page redirect to sign in" do
+    sign_out @user
+    # @request.env["devise.mapping"] = Devise.mappings[:course]
+    
+    get :new
 
-  #   get :new
-
-  #   assert_redirected_to =
-  # end
+    assert_redirected_to new_user_session_path
+  end
   
 end
--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -1,16 +1,21 @@
 require 'test_helper'
 
 class CourseTest < ActiveSupport::TestCase
+  include Devise::TestHelpers
   # test "the truth" do
   #   assert true
   # end
 
-  # setup do
-  # 	@user = FactoryGirl.create(:user)
-  # 	sign_in @user
-  # end
+  setup do
+  	@user = FactoryGirl.create(:user)
+  	sign_in @user
 
+  end
+
+  # getting undefined method 'env' for nil:NilClass
+  
   # test "should create course" do
+  #   course = Course.new(:title => 'My course title', :description => 'My course description', :cost => 29.99 )
 
   # 	assert true
   	
