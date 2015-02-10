0474732dc7868ebc181607d00d1ced26825decb4
Aaron Washburn <aaron@it-kutch.com>
2015-02-10 21:41:57 +0000

    remove require authorized from instructor/courses#create

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,6 +1,6 @@
 class Instructor::CoursesController < ApplicationController
 	before_action :authenticate_user!
-	before_action :require_authorized_for_current_course, :only => [:show, :create, :edit, :update]
+	before_action :require_authorized_for_current_course, :only => [:show, :edit, :update]
 
 	def new
 		@course = Course.new
