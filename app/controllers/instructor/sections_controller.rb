9bc6390eefd38a14c4e6b6a93d54f0a357b675cd
Aaron Washburn <aaron@it-kutch.com>
2015-02-10 09:47:31 +0000

    Refacter courses and sections controllers

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,17 +1,16 @@
 class Instructor::SectionsController < ApplicationController
 	before_action :authenticate_user!
+	before_action :require_authorized_for_current_course
 	
 	def new
 		@section = Section.new
-		@course = Course.find(params[:course_id])
 	end
 
 	def create
-		@course = Course.find(params[:course_id])
-    @section = @course.sections.create(section_params)
+		@section = current_course.sections.create(section_params)
 
 		if @section.valid?
-			redirect_to instructor_course_path(@course)
+			redirect_to instructor_course_path(current_course)
 		else
 			render :new, :status => :unprocessable_entity
 		end
@@ -21,5 +20,16 @@ class Instructor::SectionsController < ApplicationController
 
 	def section_params
 		params.require(:section).permit(:title)
+	end
+
+	helper_method :current_course
+	def current_course
+		@current_course ||= Course.find(params[:course_id])
+	end
+
+	def require_authorized_for_current_course
+		if current_course.user != current_user
+			render :text => 'Unauthorized', :status => :unauthorized
+		end
 	end
 end
