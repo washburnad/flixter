4bedb181751959d7b1da09ff20a015ce70de4e6f
Aaron Washburn <aaron@it-kutch.com>
2015-02-19 10:18:22 -0500

    Style footer

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -2,6 +2,10 @@ class Instructor::CoursesController < ApplicationController
 	before_action :authenticate_user!
 	before_action :require_authorized_for_current_course, :only => [:show, :edit, :update]
 
+	def new
+		@course = Course.new
+	end
+
 	def create
 		@course = current_user.courses.create(course_params)
 
