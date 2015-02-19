843d9134912bf3d3e74bbf46da0076d5026300e6
Aaron Washburn <aaron@it-kutch.com>
2015-02-19 10:24:06 -0500

    Add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Flixter::Application.routes.draw do
   devise_for :users
   root 'static_pages#index'
+  get 'privacy', :to => 'static_pages#privacy'
+  get 'careers', :to => 'static_pages#careers'
+  get 'team', :to => 'static_pages#team'
   resource :dashboard, :only => [:show]
   resources :courses, :only => [:index, :show] do
     resources :enrollments, :only => [:create, :destroy]
